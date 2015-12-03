8e9290d890ad910c9306688c6b016da88faaa0d8
Ritesh <riteshmmenon@gmail.com>
2015-12-03 23:36:17 +0530

    CAM-2 #comment Just a stupid comment #time 2m

--- src/main/java/camlin/service/UserService.java
+++ src/main/java/camlin/service/UserService.java
@@ -12,6 +12,7 @@ public class UserService {
 
     public User getDefaultUser() {
         User user = new User();
+        // Adding primordial user
         user.setFirstName("Ritesh");
         user.setLastName("Menon");
         return user;
